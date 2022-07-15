44a4a13e98c47d759e277e5d6b995f379ae3cb80
illiakar <illia.karbivnychyi@gmail.com>
2022-07-15 12:43:01 -0700

    second commit

--- app/src/main/java/com/example/java_basic_app3/MainActivity.java
+++ app/src/main/java/com/example/java_basic_app3/MainActivity.java
@@ -12,29 +12,58 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Integer vaultOpened = 0;
+
+    int maxWork = 0;
+
+    int wage = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (SecondActivity.vaultOpen == 1) {
+            wage = SecondActivity.wage;
+            ((TextView) findViewById(R.id.money)).setText(SecondActivity.coinCount.toString());
+            ((TextView) findViewById(R.id.wage)).setText(SecondActivity.wage.toString());
+            ((TextView) findViewById(R.id.shop_button)).setText("        VAULT (0)              ");
+        } else {
+            //pass
+        }
     }
 
     public void goToSecond(View v) {
         String countString = ((TextView) findViewById(R.id.money)).getText().toString();
         Integer count = Integer.parseInt(countString);
+        if (SecondActivity.vaultOpen == 1) {
+            //pass
+        }
 
-        if (count >= 1000) {
+        if (SecondActivity.vaultOpen == 1) {
+            vaultOpened++;
             Intent myI = new Intent(this, SecondActivity.class);
             String coins = ((TextView) findViewById(R.id.money)).getText().toString();
+            String wage = ((TextView) findViewById(R.id.wage)).getText().toString();
             myI.putExtra("COINS", coins);
+            myI.putExtra("WAGE", wage);
+            myI.putExtra("VAULT", vaultOpened.toString());
             startActivity(myI);
         } else {
-            Toast.makeText(this, "Not enough coins", Toast.LENGTH_LONG).show();
+            if (count >= 1000) {
+                vaultOpened++;
+                Intent myI = new Intent(this, SecondActivity.class);
+                String coins = ((TextView) findViewById(R.id.money)).getText().toString();
+                String wage = ((TextView) findViewById(R.id.wage)).getText().toString();
+                myI.putExtra("COINS", coins);
+                myI.putExtra("WAGE", wage);
+                myI.putExtra("VAULT", vaultOpened.toString());
+                startActivity(myI);
+            } else {
+                Toast.makeText(this, "Not enough coins", Toast.LENGTH_LONG).show();
+            }
         }
     }
-    //ab
-    int maxWork = 0;
-
-    int wage = 10;
 
     public void getMoney(View v) {
 
--- app/src/main/java/com/example/java_basic_app3/SecondActivity.java
+++ app/src/main/java/com/example/java_basic_app3/SecondActivity.java
@@ -1,27 +1,145 @@
 package com.example.java_basic_app3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button firstFragmentBtn, secondFragmentBtn;
+
+    static Integer coinCount = 0;
+
+    static Integer wage = 10;
+
+    static Integer vaultOpen = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Intent myI = getIntent();
-        Integer coinCount = Integer.parseInt(myI.getStringExtra("COINS"));
+        coinCount = Integer.parseInt(myI.getStringExtra("COINS"));
+
+        if (vaultOpen == 0) {
+            coinCount -= 1000;
+        }
+
         ((TextView) findViewById(R.id.money2)).setText(coinCount.toString());
+
+        wage = Integer.parseInt(myI.getStringExtra("WAGE"));
+
+        vaultOpen = Integer.parseInt(myI.getStringExtra("VAULT"));
+
+        firstFragmentBtn = findViewById(R.id.fragment1_button);
+
+        secondFragmentBtn = findViewById(R.id.fragment2_button);
+
+        replaceFrag(new frag1());
+
+        firstFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                replaceFrag(new frag1());
+
+            }
+        });
+
+        secondFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                replaceFrag(new frag2());
+                
+            }
+        });
+    }
+
+    private void replaceFrag(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, fragment);
+        fragmentTransaction.commit();
+
     }
 
     public void goToMain(View v) {
         Intent myAct = new Intent(this, MainActivity.class);
         startActivity(myAct);
 
+        vaultOpen = 0;
+
     }
+
+    public void goToWork(View v) {
+
+        coinCount = Integer.parseInt(((TextView) findViewById(R.id.money2)).getText().toString());
+
+        Intent myAct = new Intent(this, MainActivity.class);
+        startActivity(myAct);
+
+
+
+    }
+
+    public void buyCandyBar(View v) {
+        String countString = ((TextView) findViewById(R.id.money2)).getText().toString();
+        Integer count = Integer.parseInt(countString);
+        if (count >= 3) {
+            Toast.makeText(this, "Thank you for your purchase!", Toast.LENGTH_LONG).show();
+            count -= 3;
+            ((TextView) findViewById(R.id.money2)).setText(count.toString());
+        } else {
+            Toast.makeText(this, "Not enough coins", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void buyIceCream(View v) {
+        String countString = ((TextView) findViewById(R.id.money2)).getText().toString();
+        Integer count = Integer.parseInt(countString);
+        if (count >= 5) {
+            Toast.makeText(this, "Thank you for your purchase!", Toast.LENGTH_LONG).show();
+            count -= 5;
+            ((TextView) findViewById(R.id.money2)).setText(count.toString());
+        } else {
+            Toast.makeText(this, "Not enough coins", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void buyFruitCup(View v) {
+        String countString = ((TextView) findViewById(R.id.money2)).getText().toString();
+        Integer count = Integer.parseInt(countString);
+        if (count >= 9) {
+            Toast.makeText(this, "Thank you for your purchase!", Toast.LENGTH_LONG).show();
+            count -= 9;
+            ((TextView) findViewById(R.id.money2)).setText(count.toString());
+        } else {
+            Toast.makeText(this, "Not enough coins", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void buyBrownies(View v) {
+        String countString = ((TextView) findViewById(R.id.money2)).getText().toString();
+        Integer count = Integer.parseInt(countString);
+        if (count >= 7) {
+            Toast.makeText(this, "Thank you for your purchase!", Toast.LENGTH_LONG).show();
+            count -= 7;
+            ((TextView) findViewById(R.id.money2)).setText(count.toString());
+        } else {
+            Toast.makeText(this, "Not enough coins", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
